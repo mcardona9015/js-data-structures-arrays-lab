08372f863bc94f79a5e60ed52d65cf81798022dc
mcardona9015 <mcardona9015@gmail.com>
2021-01-30 17:52:08 -0500

    commit tree 88ad87873f5152a857ed593f4c83c42553cb0eaa

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+// drivers.push("Ralph");
+
+// drivers.shift("Bob");
+
+// drivers.pop();
+
+// drivers.unshift();
+
+// // const newDriver = [...drivers, "Broom"];
+
+// // const newDriver = ["Arnold", ...drivers];
+
+drivers.slice(0, 2);
+
+drivers.slice(-2);
